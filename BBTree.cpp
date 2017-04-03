a79d1f8fa1cd2d9909f5dc5864151c6b7b2b5569
sbenz <sbenz.96@gmx.de>
2017-04-03 22:23:23 +0200

    Added empty implementations of BBTree.h function

--- BBTree.cpp
+++ BBTree.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 #include "BBTree.h"
+
+bool BBTree::Node::search(const int) const {
+    return false;
+}
+
+BBTree::Node *BBTree::Node::insert(const int, const double) {
+    return nullptr;
+}
+
+BBTree::Node *BBTree::Node::remove(const int, const double) {
+    return nullptr;
+}
+
+bool BBTree::search(const int) const {
+    return false;
+}
+
+void BBTree::insert(const int) {
+
+}
+
+void BBTree::remove(const int) {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "BBTree.h"
+#include "BBTree.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
